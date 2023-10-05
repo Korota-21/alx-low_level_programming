8b327263ce673fd74eb3543fa80afabd71a371cd
Marwa <koroko123430@gmail.com>
2023-10-05 11:17:08 +0300

    0x0A

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc : first argument
+ * @argv:  secont argunment
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+int count;
+int add = 0;
+if (argc > 1)
+{
+for (count = 1; count < argc; count++)
+{
+if (atoi(argv[count]))
+{
+
+	add += atoi(argv[count]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+	}
+printf("%d\n", add);
+}
+else
+{
+printf("0\n");
+}
+return (0);
+
+}
