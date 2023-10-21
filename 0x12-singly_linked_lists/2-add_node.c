70f16ad628cbcb6759fb8a3ab9b8fc1d2c46ee8c
Marwa <koroko123430@gmail.com>
2023-10-21 05:25:10 +0300

    0x0D 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,39 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
-
-
 /**
- * add_node -add node to list.
- *Description: Function that adds a new node at the beginning of a list_t list
- * @head: pointer to a pointer
- * @str: string value in the structure
- * Return: the address of the new element or NULL if fail
- **/
-
+ * add_node - adds a new node at the beginning of a list
+ *@head: pointer to the head of the list
+ *@str: string to be added
+ *
+ * Return: returns the address to the new element or NULL
+ * if failed
+*/
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-{
-	return (NULL);
-}
-	new_node->str = strdup(str);
-	new_node->next = (*head);
-	(*head) = new_node;
+	char *dup;
+	int len;
+	list_t *new;
 
-return (new_node);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -22,8 +25,7 @@ int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif
 
+#endif
