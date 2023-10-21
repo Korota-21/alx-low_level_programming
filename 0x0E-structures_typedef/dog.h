97dd800dcd3879f5056f0a366c4afecc44c4266b
Marwa <koroko123430@gmail.com>
2023-10-21 06:09:57 +0300

    4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * dog_t - Typedef for struct dog
  */
@@ -27,5 +30,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif
